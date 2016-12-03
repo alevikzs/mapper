49590da0f8e8d94a2577d7034130036b65a5d3cc
alexey novikov <alekseeey@gmail.com>
2016-12-03 22:50:37 +0200

    add phpunit and init structure

--- src/Base.php
+++ src/Base.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksey
- * Date: 03.12.16
- * Time: 22:32
- */
 
 namespace Mapper;
-
 
 class Base
 {
--- tests/Test.php
+++ tests/Test.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksey
- * Date: 03.12.16
- * Time: 22:02
- */
 
 namespace Tests;
 
-
-use MainTest;
-
-
-class Test extends \PHPUnit_Framework_TestCase
-{
+class Test extends \PHPUnit_Framework_TestCase {
 
 }
