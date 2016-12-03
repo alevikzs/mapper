f0d0a35c2ada0a50f7e6ecc11dca0ed1a4a100ab
alexey novikov <alekseeey@gmail.com>
2016-12-03 22:49:36 +0200

    add init structure and phpunit

---
+++ src/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksey
+ * Date: 03.12.16
+ * Time: 22:32
+ */
+
+namespace Mapper;
+
+
+class Base
+{
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksey
+ * Date: 03.12.16
+ * Time: 22:02
+ */
+
+namespace Tests;
+
+
+use MainTest;
+
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+
+}
