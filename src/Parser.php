68c5a8c2ae3e4fc13cff80e5c8de03ea5a03c059
alexey novikov <alekseeey@gmail.com>
2016-12-06 23:34:20 +0200

    updated Parser

--- src/Parser.php
+++ src/Parser.php
@@ -3,12 +3,18 @@
 namespace Mapper;
 
 use \ReflectionClass;
+use \ReflectionMethod;
 
 /**
  * Class Parser
  * @package Mapper
  */
 class Parser {
+
+    /**
+     * @const string
+     */
+    const PARAM_SETTER_PATTERN = '/@param\s+(.+)/';
 
     /**
      * @var object
@@ -21,17 +27,16 @@ class Parser {
     private $reflectionClass;
 
     /**
-     * Parser constructor.
      * @param object $object
      */
-    public function __construct(object $object) {
+    public function __construct($object) {
         $this->setObject($object);
     }
 
     /**
      * @return object
      */
-    public function getObject(): object {
+    public function getObject() {
         return $this->object;
     }
 
@@ -39,7 +44,7 @@ class Parser {
      * @param object $object
      * @return Parser
      */
-    public function setObject(object $object): Parser {
+    public function setObject($object): Parser {
         $this->object = $object;
 
         return $this->setReflectionClass(new ReflectionClass($object));
@@ -62,6 +67,69 @@ class Parser {
         return $this;
     }
 
+    /**
+     * @return Setter[]
+     */
+    public function getSetters(): array {
+        $methods = $this->getReflectionClass()->getMethods();
 
+        $setters = [];
+
+        foreach ($methods as $method) {
+            if ($this->isSetterMethod($method)) {
+                $setters[] = $this->createSetter($method);
+            }
+        }
+
+        return $setters;
+    }
+
+    /**
+     * @param ReflectionMethod $method
+     * @return bool
+     */
+    private function isSetterMethod(ReflectionMethod $method): bool {
+        $name = $method->getName();
+
+        return strpos($name, 'set') !== false;
+    }
+
+    /**
+     * @param ReflectionMethod $method
+     * @return Setter
+     */
+    private function createSetter(ReflectionMethod $method): Setter {
+        $fieldName = lcfirst(str_replace('set', '', $method->getName()));
+
+        preg_match(self::PARAM_SETTER_PATTERN, $method->getDocComment(), $paramTypeAndVariable);
+
+        $isArray = false;
+        $type = null;
+
+        if (isset($paramTypeAndVariable[1])) {
+            $paramParts = preg_split('/\s+/', $paramTypeAndVariable[1], 3, PREG_SPLIT_DELIM_CAPTURE);
+            $type = null;
+            foreach ($paramParts as $paramPart) {
+                if ($paramPart[0] !== '$') {
+                    $isArray = false;
+                    $type = $paramPart;
+                    if (strpos($paramPart, '[]') !== false) {
+                        $isArray = true;
+                        $type = str_replace('[]', '', $type);
+                    } elseif ($type === 'array') {
+                        $isArray = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return new Setter(
+            $fieldName,
+            $method->getName(),
+            $type,
+            $isArray
+        );
+    }
 
 }
