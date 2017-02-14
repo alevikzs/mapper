b3dfc35787fc47513dd5da2d4513c5bb61d29102
alexey novikov <alekseeey@gmail.com>
2017-02-15 00:20:55 +0200

    added ClassFieldsTest

---
+++ tests/ClassFieldsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+
+use \Mapper\ClassField;
+use \Mapper\ClassFields;
+
+/**
+ * Class ClassFieldsTest
+ * @package Tests
+ */
+class ClassFieldsTest extends TestCase {
+
+    public function testMain() {
+        $setter = 'setBranch';
+        $field = 'branch';
+
+        $classField = new ClassField($setter, $field);
+        $classFields = (new ClassFields())->add($classField);
+
+        $wrongField = 'wrongField';
+
+        $this->assertTrue($classFields->hasClassField($field));
+        $this->assertFalse($classFields->hasClassField($wrongField));
+
+        $this->assertEquals($classField, $classFields->getClassField($field));
+        $this->assertNull($classFields->getClassField($wrongField));
+    }
+
+}
